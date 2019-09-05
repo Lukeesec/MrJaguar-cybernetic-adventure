ee4570bf8269fc2227f79ad61845d60d3b27be61
root <lukemartin1968@yahoo.com>
2019-09-05 09:08:22 -0400

    Removed HTB

--- htb/retired_machines/teacher_PWNED/wordlist_script.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-test = 'Th4C00lTheacha'
-char_set = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'
-for i in char_set:
-	with open('python_wordlist','a') as f:
-		f.write(test+i+'\n')
